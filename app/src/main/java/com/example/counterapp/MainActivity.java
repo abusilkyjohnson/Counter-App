4c509a93cfc01e1cce1182ea2a5ee676f4a8bd94
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-01 11:03:18 -0500

    add the boolean count butt properly

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.counterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +18,25 @@ public class MainActivity extends AppCompatActivity {
         TextView startingNum = findViewById(R.id.startingNum);
 
         int zeroNumStart = 0;
-        boolean buttonTrigger = false;
+        final boolean[] buttonTrigger = {false};
+        Button countButtVar = findViewById(R.id.countButt);
 
-        while(buttonTrigger)
+        countButtVar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(buttonTrigger[0] == false)
+                {
+                    buttonTrigger[0] = true;
+                }
+                else if (buttonTrigger[0] == true)
+                {
+                    buttonTrigger[0] = false;
+                }
+            }
+        });
+
+
+        while(buttonTrigger[0])
         {
             zeroNumStart++;
             Integer.toString(zeroNumStart);
